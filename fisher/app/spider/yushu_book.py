b17b518461abe2e312d7cad0e3dcb2cd48bae5a1
Chauncey2 <gaochangju95@163.com>
2020-01-11 23:34:41 +0800

    Add ViewModel level

--- fisher/app/spider/yushu_book.py
+++ fisher/app/spider/yushu_book.py
@@ -5,21 +5,35 @@ class YuShuBook:
     isbn_url='http://t.yushu.im/v2/book/isbn/{}'
     keyword_url='http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
-    @classmethod
-    def search_by_isbn(cls,isbn):
-        url=cls.isbn_url.format(isbn)
+    def __init__(self):
+        self.total=0
+        self.books=[]
+
+
+    def search_by_isbn(self,isbn):
+        url=self.isbn_url.format(isbn)
         result=HTTP.get(url)
         return result
 
-    @classmethod
-    def search_by_keyword(cls,keyword,page):
-        url=cls.keyword_url.format(keyword,current_app.config['PER_PAGE'],
-                                   cls.calculate_start(page))
+
+    def __fill_single(self,data):
+        if data:
+            self.total = 1
+            self.books.append(data)
+
+    def __fill_collection(self,data):
+        self.total=data['total']
+        self.books=data['books']
+
+
+    def search_by_keyword(self,keyword,page):
+        url=self.keyword_url.format(keyword,current_app.config['PER_PAGE'],
+                                   self.calculate_start(page))
         result=HTTP.get(url)
         return result
 
-    @staticmethod
-    def calculate_start(page):
+
+    def calculate_start(self,page):
         return (page-1)*current_app.config['PER_PAGE']
 
 
