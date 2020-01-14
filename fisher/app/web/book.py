7a4f7b5c30425d44236cbbb5811c5aa7cfa75306
Chauncey2 <gaochangju95@163.com>
2020-01-14 23:51:40 +0800

    Add ViewModel level

--- fisher/app/web/book.py
+++ fisher/app/web/book.py
@@ -1,11 +1,12 @@
-from flask import jsonify,request
+from flask import jsonify, request, render_template, flash
 
-from app.view_models.book import  BookCollection
+from app.view_models.book import BookCollection
 from . import web
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
 import json
+
 
 @web.route('/book/search')
 def search():
@@ -21,15 +22,22 @@ def search():
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
-        isbn_or_key=is_isbn_or_key(q)
+        isbn_or_key = is_isbn_or_key(q)
         yushu_book = YuShuBook()
 
-        if isbn_or_key=='isbn':
+        if isbn_or_key == 'isbn':
             yushu_book.search_by_isbn(q)
         else:
-            yushu_book.search_by_keyword(q,page)
+            yushu_book.search_by_keyword(q, page)
 
-        books.fill(yushu_book,q)
+        books.fill(yushu_book, q)
         return json.dumps(books, default=lambda obj: obj.__dict__)
     else:
-        return jsonify(form.err)
+        return jsonify(form.error)
+    return render_template('search_result.html', books=books)
+
+
+@web.route('/test')
+def test():
+    flash("hello,flash")
+    return render_template('test.html')
